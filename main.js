7a7d55520c658fe48d3e62f72e5f49dcdbe6e170
Thitaree <thitaree.lav@dome.tu.ac.th>
2021-02-08 16:05:28 +0700

    Refactor code

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function sum(a, b){
     if (Array.isArray(a)){
         let total = 0
-        for (let i=0; i<a.length; i++){
-            total += a[i]
-            }
+        a.forEach((i) -> (total += i))
             return total
          }
     return a + b
